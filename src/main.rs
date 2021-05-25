ee103fb2fc7cdef0452c5aab41df8d76bb11b808
Anshuman <amedhi@connect.ust.hk>
2021-05-25 15:16:50 +0800

    implement a bunch

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+#![feature(array_chunks)]
+#![feature(with_options)]
+
+use std::ffi::OsString;
 use std::fs::File;
 use std::io;
 use std::io::{stdout, BufWriter, Write};
 use std::process::exit;
+use std::str;
+use std::str::FromStr;
 use std::time::Duration;
+use std::time::Instant;
 
 use serialport::{DataBits, FlowControl, Parity, SerialPort, StopBits};
 
@@ -25,13 +32,60 @@ OPTIONS:
 	-s --stop-bits    NUMBER     Number of stop bits (1, 2) [default: 1]
 	-T --timeout      NUMBER     Timeout (milliseconds) on receiving data [default: 0]
     -m --mode         STRING     stdout, iterm, lines, file  [default: stdout]
-    -o --output-file  PATH       File to write to (only relevant with `-m file`) (unimplemented)
-    -c --capacity     NUMBER     Buffer capacity for stdout/file writing [default: 64] 
+    -o --output-file  PATH       File to write to (only relevant with `-m file`) [default: output.txt]
+    -a --append       PATH       In file mode we will append the new data [default: false]
+    -C --capacity     NUMBER     Buffer capacity for stdout/file writing [default: 64] 
         This is set quite low to be able to see live updates (and because UART is slow usually)
         You could increase this a lot if you are writing to a file and/or using faster data
+    -c --convert      STRING     Perform some simple data conversion (all outputs human readable ascii)
+        (NO OPT) just view/save the data, this essentially means ASCII
+        HEX  convert every byte to hex representation
+        BIN  convert every byte to binary representation
+        INT  convert every 4 bytes from 32 bit integers 
+        SHR  convert every 2 bytes from 16 bit integers 
+        U*   unsigned variants of the above 2
+        FLT  convert every 4 bytes from 32 bit floating points
     -f --format       PATH       Path to file with parser format (unimplemented)
         Parse binary data into human-readable format for more efficient bandwidth usage
 ";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ConvertFrom {
+    NON,
+    HEX,
+    BIN,
+    INT,
+    UINT,
+    USHR,
+    SHR,
+    FLT,
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct InvalidConvertFrom;
+
+impl std::fmt::Display for InvalidConvertFrom {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("ConvertFrom got invalid value")
+    }
+}
+impl FromStr for ConvertFrom {
+    type Err = InvalidConvertFrom;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use ConvertFrom::*;
+        Ok(match s {
+            "NON" => NON,
+            "HEX" => HEX,
+            "BIN" => BIN,
+            "INT" => INT,
+            "SHR" => SHR,
+            "UINT" => UINT,
+            "USHR" => USHR,
+            "FLT" => FLT,
+            _ => return Err(InvalidConvertFrom),
+        })
+    }
+}
 
 #[derive(Debug)]
 struct Args {
@@ -44,6 +98,9 @@ struct Args {
     timeout: Duration,
     mode: String,
     capacity: usize,
+    output_file: OsString,
+    append: bool,
+    convertfrom: ConvertFrom,
 }
 impl Default for Args {
     fn default() -> Self {
@@ -57,6 +114,9 @@ impl Default for Args {
             timeout: Duration::from_millis(0),
             mode: String::from("stdout"),
             capacity: 64,
+            output_file: "output.txt".into(),
+            append: false,
+            convertfrom: ConvertFrom::NON,
         }
     }
 }
@@ -89,10 +149,12 @@ fn main() {
                     exit(0)
                 }
             }),
+
         baud_rate: pargs
             .opt_value_from_str(["-b", "--baud-rate"])
             .unwrap()
             .unwrap_or(dargs.baud_rate),
+
         data_bits: pargs
             .opt_value_from_fn(["-d", "--data-bits"], |x| {
                 Ok(match x {
@@ -105,6 +167,7 @@ fn main() {
             })
             .unwrap()
             .unwrap_or(dargs.data_bits),
+
         flow_control: pargs
             .opt_value_from_fn(["-F", "--flow-control"], |n| {
                 Ok(match n.to_ascii_lowercase().as_str() {
@@ -116,6 +179,7 @@ fn main() {
             })
             .unwrap()
             .unwrap_or(dargs.flow_control),
+
         parity: pargs
             .opt_value_from_fn(["-P", "--parity"], |n| {
                 Ok(match n.to_ascii_lowercase().as_str() {
@@ -127,6 +191,7 @@ fn main() {
             })
             .unwrap()
             .unwrap_or(dargs.parity),
+
         stop_bits: pargs
             .opt_value_from_fn(["-s", "--stop-bits"], |n| {
                 Ok(match n.to_ascii_lowercase().as_str() {
@@ -137,20 +202,39 @@ fn main() {
             })
             .unwrap()
             .unwrap_or(dargs.stop_bits),
+
         timeout: pargs
             .opt_value_from_str(["-T", "--timeout"])
             .unwrap()
             .map(|t| Duration::from_millis(t))
             .unwrap_or(dargs.timeout),
+
         mode: pargs
             .opt_value_from_str(["-w", "--mode"])
             .unwrap()
             .unwrap_or(dargs.mode),
+
         capacity: pargs
-            .opt_value_from_str(["-c", "--capacity"])
+            .opt_value_from_str(["-C", "--capacity"])
             .unwrap()
             .unwrap_or(dargs.capacity),
-        ..dargs
+
+        output_file: pargs
+            .opt_value_from_os_str::<_, _, &'static str>(["-o", "--output-file"], |s| {
+                Ok(s.to_owned())
+            })
+            .unwrap()
+            .unwrap_or(dargs.output_file),
+
+        append: pargs
+            .opt_value_from_str(["-a", "--append"])
+            .unwrap()
+            .unwrap_or(dargs.append),
+
+        convertfrom: pargs
+            .opt_value_from_str(["-c", "--convert"])
+            .unwrap()
+            .unwrap_or(dargs.convertfrom),
     };
 
     if pargs.contains(["-o", "--output-file"]) {
@@ -174,46 +258,166 @@ fn main() {
         .open()
         .expect("Could not open the serial port");
 
-    if args.mode.eq_ignore_ascii_case("stdout") {
-        serial_to_writer(port, stdout().lock(), false, &args)
-    } else if args.mode.eq_ignore_ascii_case("iterm") {
-        serial_iterm(port, &args)
-    } else if args.mode.eq_ignore_ascii_case("lines") {
-        serial_line_interactive(port, &args)
-    } else if args.mode.eq_ignore_ascii_case("file") {
-        serial_to_writer(port, File::open("output.txt").unwrap(), true, &args)
+    match args.mode.to_lowercase().as_str() {
+        "stdout" => serial_to_writer(port, stdout().lock(), false, &args),
+        "iterm" => serial_iterm(port, &args),
+        "lines" => serial_line_interactive(port, &args),
+        "file" => serial_to_writer(
+            port,
+            File::with_options()
+                .write(true)
+                .append(true)
+                .create(true)
+                .open(args.output_file.as_os_str())
+                .unwrap(),
+            // File::open(args.output_file.as_os_str()).unwrap(),
+            true,
+            &args,
+        ),
+        _ => {
+            println!("Invalid serial mode passed");
+        }
     }
-    todo!("Implement the tui");
 }
 
 fn serial_line_interactive(port: Box<dyn SerialPort>, args: &Args) {
-    todo!()
+    todo!("Interactive mode not implemented yet")
 }
 
 fn serial_iterm(port: Box<dyn SerialPort>, args: &Args) {
-    todo!()
+    todo!("Terminal mode not implemented yet")
 }
 
 /// Read only streaming from the serial port
 /// Writes the data to the Write object (buffered for performance)
 fn serial_to_writer(mut port: Box<dyn SerialPort>, out: impl Write, counts: bool, args: &Args) {
     let mut out = BufWriter::with_capacity(args.capacity, out);
+    let mut buf = Vec::with_capacity(args.capacity);
+    let buf = &mut buf;
 
-    loop {
-        match io::copy(&mut port, &mut out) {
+    let mut stamp = Instant::now();
+
+    let mut count_words = 0;
+    let mut count_commas = 0;
+    let mut count_bytes = 0;
+    let mut count_lines = 0;
+
+    let mut copy = move || -> Result<_, _> {
+        match port.read(buf) {
             Ok(n) => {
-                if counts {
-                    // TODO: count bytes, bytes/sec, lines, etc
-                    println!("{}", n);
+                count_bytes += n;
+                match args.convertfrom {
+                    ConvertFrom::NON => {
+                        let (words, commas, lines) = buf.iter().fold(
+                            (count_words, count_commas, count_lines),
+                            |(w, c, l), &b| match b {
+                                b' ' => (w + 1, c, l),
+                                b'\n' => (w, c, l + 1),
+                                b',' => (w, c + 1, l),
+                                _ => (w, c, l),
+                            },
+                        );
+                        count_words = words;
+                        count_commas = commas;
+                        count_lines = lines;
+                    }
+                    ConvertFrom::HEX => {
+                        let mut out = Vec::with_capacity(buf.len() * 2);
+                        for byte in &*buf {
+                            write!(out, "{:x}", byte)?;
+                        }
+                        *buf = out;
+                    }
+                    ConvertFrom::BIN => {
+                        let mut out = Vec::with_capacity(buf.len() * 8);
+                        for byte in &*buf {
+                            write!(out, "{:b}", byte)?;
+                        }
+                        *buf = out;
+                    }
+                    ConvertFrom::INT => {
+                        let mut out = Vec::with_capacity(buf.len() * 4);
+                        for &bytes in buf.array_chunks() {
+                            write!(out, "{}", i32::from_le_bytes(bytes))?;
+                        }
+                        *buf = out;
+                    }
+                    ConvertFrom::SHR => {
+                        let mut out = Vec::with_capacity(buf.len() * 2);
+                        for &bytes in buf.array_chunks() {
+                            write!(out, "{}", i16::from_le_bytes(bytes))?;
+                        }
+                        *buf = out;
+                    }
+                    ConvertFrom::FLT => {
+                        let mut out = Vec::with_capacity(buf.len() * 4);
+                        for &bytes in buf.array_chunks() {
+                            // TODO: floating point decimal points??
+                            write!(out, "{}", f32::from_le_bytes(bytes))?;
+                        }
+                        *buf = out;
+                    }
+                    ConvertFrom::UINT => {
+                        let mut out = Vec::with_capacity(buf.len() * 4);
+                        for &bytes in buf.array_chunks() {
+                            write!(out, "{}", u32::from_le_bytes(bytes))?;
+                        }
+                        *buf = out;
+                    }
+                    ConvertFrom::USHR => {
+                        let mut out = Vec::with_capacity(buf.len() * 2);
+                        for &bytes in buf.array_chunks() {
+                            write!(out, "{}", u16::from_le_bytes(bytes))?;
+                        }
+                        *buf = out;
+                    }
                 }
             }
             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-            Err(e) => serial_read_error(&port, e),
+            Err(e) => return Err(e),
         }
+
+        out.write_all(&buf)?; // TODO: I think this should exit the program
+
+        let now = Instant::now();
+        let time = now - stamp;
+        if time.as_secs() >= 1 {
+            count_words = 0;
+            count_commas = 0;
+            count_bytes = 0;
+            count_lines = 0;
+            println!(
+                "w{}, c{}, b{}, l{}",
+                count_words as f64 / time.as_secs_f64(),
+                count_commas as f64 / time.as_secs_f64(),
+                count_bytes as f64 / time.as_secs_f64(),
+                count_lines as f64 / time.as_secs_f64(),
+            );
+            stamp = now;
+        }
+
+        Ok(())
+    };
+
+    loop {
+        match copy() {
+            Ok(_) => {}
+            Err(e) => serial_read_error(e),
+        };
+        // match io::copy(&mut port, &mut out) {
+        //     Ok(n) => {
+        //         if counts {
+        //             // TODO: count bytes, bytes/sec, lines, etc
+        //             println!("{}", n);
+        //         }
+        //     }
+        //     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+        //     Err(e) => serial_read_error(&port, e),
+        // }
     }
 }
 
-fn serial_read_error(port: &Box<dyn SerialPort>, e: std::io::Error) {
+fn serial_read_error(e: std::io::Error) {
     // TODO: better error messages
     eprintln!("{:?}", e);
     // TODO: Should exit/panic?
